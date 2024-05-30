dfbd43b8cbbf51e26d29b1a7e1a4406b3f82ca8e
Amey Sawalkar <ameykishor@gmail.com>
2024-05-30 15:05:49 +0530

    BookStoreDone

---
+++ footer.php
@@ -0,0 +1,39 @@
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php">HOME</a>
+         <a href="about.php">ABOUT</a>
+         <a href="shop.php">SHOP</a>
+         <a href="contact.php">CONTACT</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="login.php">LOGIN</a>
+         <a href="register.php">REGISTER</a>
+         <a href="cart.php">CART</a>
+         <a href="orders.php">ORDERS</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <p> <i class="fas fa-phone"></i> +111-222-3333 </p>
+         <p> <i class="fas fa-envelope"></i> bookly@gmail.com </p>
+         <p> <i class="fas fa-map-marker-alt"></i>  Pune, India 440-001 </p>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> Facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> Twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> Instagram </a>
+         <a href="#"> <i class="fab fa-linkedin"></i> LinkedIn </a>
+      </div>
+
+   </div>
+
+
+</section>
